9086108a2152f2649936ecf237f8b7e00cb1e13d
YnwCh3f <benacsekd07@gmail.com>
2024-04-10 21:53:49 +0200

    shuffle

--- src/main/java/com/example/blackjack/HelloController.java
+++ src/main/java/com/example/blackjack/HelloController.java
@@ -1,8 +1,34 @@
 package com.example.blackjack;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
 
+    public void initialize(){
+        //Server s = new Server();
+    }
+
+    public void onHitClick(ActionEvent actionEvent) {
+    }
+
+    public void onStandClick(ActionEvent actionEvent) {
+    }
+
+    public void onLeaveClick(ActionEvent actionEvent) {
+    }
+
+    public void stake5(MouseEvent mouseEvent) {
+    }
+
+    public void stake25(MouseEvent mouseEvent) {
+    }
+
+    public void stake50(MouseEvent mouseEvent) {
+    }
+
+    public void stake100(MouseEvent mouseEvent) {
+    }
 }
