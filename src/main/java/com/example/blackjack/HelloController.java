2913202222085316aa8eb37ed3106f9a00288a9c
StormiHUN <stormihun@gmail.com>
2024-04-12 13:05:18 +0200

    Visuals

--- src/main/java/com/example/blackjack/HelloController.java
+++ src/main/java/com/example/blackjack/HelloController.java
@@ -1,8 +1,51 @@
 package com.example.blackjack;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Arc;
 
 public class HelloController {
 
+    public Arc table;
+    public Label bank;
+    public RadioButton isClient;
+    public ToggleGroup modes;
+    public TextField serverIp;
+
+    public void initialize(){
+
+    }
+
+    public void stake5(MouseEvent mouseEvent) {
+
+    }
+
+    public void stake25(MouseEvent mouseEvent) {
+
+    }
+
+    public void stake50(MouseEvent mouseEvent) {
+
+    }
+
+    public void stake100(MouseEvent mouseEvent) {
+
+    }
+
+    public void onHitClick(ActionEvent actionEvent) {
+
+    }
+
+    public void onStandClick(ActionEvent actionEvent) {
+
+    }
+
+    public void onLeaveClick(ActionEvent actionEvent) {
+
+    }
 }
