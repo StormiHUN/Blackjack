c16d3f2313507daaa62dba86e3fe9d91b859c0cd
StormiHUN <stormihun@gmail.com>
2024-04-12 13:16:36 +0200

    Visuals

--- src/main/java/com/example/blackjack/HelloController.java
+++ src/main/java/com/example/blackjack/HelloController.java
@@ -2,10 +2,7 @@ package com.example.blackjack;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Arc;
 
@@ -16,36 +13,52 @@ public class HelloController {
     public RadioButton isClient;
     public ToggleGroup modes;
     public TextField serverIp;
+    public Button hitButton;
+    public Button standButton;
+    public Button allInButton;
+    public Button leaveButton;
 
     public void initialize(){
+
+
 
     }
 
     public void stake5(MouseEvent mouseEvent) {
 
+
     }
 
     public void stake25(MouseEvent mouseEvent) {
+
 
     }
 
     public void stake50(MouseEvent mouseEvent) {
 
+
     }
 
     public void stake100(MouseEvent mouseEvent) {
+
 
     }
 
     public void onHitClick(ActionEvent actionEvent) {
 
+
     }
 
     public void onStandClick(ActionEvent actionEvent) {
+
 
     }
 
     public void onLeaveClick(ActionEvent actionEvent) {
 
+
+    }
+
+    public void onAllInClick(ActionEvent actionEvent) {
     }
 }
